f7ca0f438860f3ffe48f25bc220aca2c975f52e8
7anish <anissh946@gmail.com>
2024-11-08 13:45:00 +0600

    url

--- adminportal/Url.js
+++ adminportal/Url.js
@@ -1,3 +1,3 @@
-// const Url = "https://medicineholesaler-production.up.railway.app"
-const Url = "http://localhost:8000"
+const Url = "https://medicineholesaler-production.up.railway.app"
+// const Url = "http://localhost:8000"
 export default Url
